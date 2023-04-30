5fc55b7138817b03d8cc3492789d75222706647e
hinto.janai <hinto.janai@protonmail.com>
2023-04-30 14:37:01 -0400

    date: reduce code bloat
    `YEAR-MM-DD` are now matched 3 times instead of duplicating
    `MM-DD` for every `YEAR`.
    
    This may be _slightly_ slower but also _heavily_ reduces
    final binary size and compile times.

--- date/src/lib.rs
+++ date/src/lib.rs
@@ -2,58 +2,38 @@
 //!
 //! Do not use this crate directly.
 
-mod _1900;
-mod _1910;
-mod _1920;
-mod _1930;
-mod _1940;
-mod _1950;
-mod _1960;
-mod _1970;
-mod _1980;
-mod _1990;
-mod _2000;
-mod _2010;
-mod _2020;
-mod _2030;
-mod _2040;
-mod _2050;
-mod _2060;
-mod _2070;
-mod _2080;
-mod _2090;
+mod year;
+mod month;
+mod day;
 
 //---------------------------------------------------------------------------------------------------- Inlined `1900-2100`
-// These functions are split up because LLVM
-// will spend actual hours churning on a single
-// function 10k+ lines long.
-
 /// # INVARIANT
 /// Input must be [u8; 10].
 ///
 pub const fn inlined(bytes: &[u8]) -> Option<(u16, u8, u8, [u8; 10])> {
+	let y = match crate::year::year(&bytes) {
+		Some(y) => y,
+		_ => return None,
+	};
+
+	let m = match crate::month::month(&bytes) {
+		Some(m) => m,
+		_ => return None,
+	};
+
+	let d = match crate::day::day(&bytes) {
+		Some(m) => m,
+		_ => return None,
+	};
+
 	// `-` is `45` in UTF-8 encoding.
-	match bytes {
-		[49, _, 48, _, _, _, _, _, _, _] => Some(crate::_1900::inlined_1900(bytes)),
-		[49, _, 49, _, _, _, _, _, _, _] => Some(crate::_1910::inlined_1910(bytes)),
-		[49, _, 50, _, _, _, _, _, _, _] => Some(crate::_1920::inlined_1920(bytes)),
-		[49, _, 51, _, _, _, _, _, _, _] => Some(crate::_1930::inlined_1930(bytes)),
-		[49, _, 52, _, _, _, _, _, _, _] => Some(crate::_1940::inlined_1940(bytes)),
-		[49, _, 53, _, _, _, _, _, _, _] => Some(crate::_1950::inlined_1950(bytes)),
-		[49, _, 54, _, _, _, _, _, _, _] => Some(crate::_1960::inlined_1960(bytes)),
-		[49, _, 55, _, _, _, _, _, _, _] => Some(crate::_1970::inlined_1970(bytes)),
-		[49, _, 56, _, _, _, _, _, _, _] => Some(crate::_1980::inlined_1980(bytes)),
-		[49, _, 57, _, _, _, _, _, _, _] => Some(crate::_1990::inlined_1990(bytes)),
-		[50, _, 48, _, _, _, _, _, _, _] => Some(crate::_2000::inlined_2000(bytes)),
-		[50, _, 49, _, _, _, _, _, _, _] => Some(crate::_2010::inlined_2010(bytes)),
-		[50, _, 50, _, _, _, _, _, _, _] => Some(crate::_2020::inlined_2020(bytes)),
-		[50, _, 51, _, _, _, _, _, _, _] => Some(crate::_2030::inlined_2030(bytes)),
-		[50, _, 52, _, _, _, _, _, _, _] => Some(crate::_2040::inlined_2040(bytes)),
-		[50, _, 53, _, _, _, _, _, _, _] => Some(crate::_2050::inlined_2050(bytes)),
-		[50, _, 54, _, _, _, _, _, _, _] => Some(crate::_2060::inlined_2060(bytes)),
-		[50, _, 55, _, _, _, _, _, _, _] => Some(crate::_2070::inlined_2070(bytes)),
-		[50, _, 56, _, _, _, _, _, _, _] => Some(crate::_2080::inlined_2080(bytes)),
-		[50, _, 57, _, _, _, _, _, _, _] => Some(crate::_2090::inlined_2090(bytes)),
-		_ => None,
-	}
+	Some((y.0, m.0, d.0,
+		[
+			y.1[0], y.1[1], y.1[2], y.1[3],
+			45,
+			m.1[0], m.1[1],
+			45,
+			d.1[0], d.1[1],
+		]
+	))
 }
